5fa984367f8da92ce1e1b0c6a934aa28010361b6
cootot <144215621+cootot@users.noreply.github.com>
2024-11-03 13:29:24 +0530

    Update hashmap.py

--- hashmap.py
+++ hashmap.py
@@ -7,6 +7,14 @@ class Entry:
     def __init__(self, k, v):
         self._key = k
         self._value = v
+    
+    def __eq__(self, other):
+        if isinstance(other, Entry):
+            return self._key == other._key
+        return False
+    
+    def __hash__(self):
+        return hash(self._key)
 
 class UnsortedTableMap:
     """Map implementation using an unordered list."""
@@ -32,12 +40,25 @@ class UnsortedTableMap:
             if k == self._table[j]._key:
                 return self._table.pop(j)._value
         raise KeyError('Key Error: ' + repr(k))
+    
+    def __contains__(self, k):
+        try:
+            self.get(k)
+            return True
+        except KeyError:
+            return False
         
     def __len__(self):
         return len(self._table)
         
     def items(self):
         return [(item._key, item._value) for item in self._table]
+    
+    def keys(self):
+        return [item._key for item in self._table]
+    
+    def values(self):
+        return [item._value for item in self._table]
 
 class ChainHashMap:
     """Hash map implemented with separate chaining for collision resolution."""
@@ -78,6 +99,20 @@ class ChainHashMap:
             # resize if load factor exceeds threshold
             if self._n > len(self._table) * self._load_factor_threshold:
                 self._resize(2 * len(self._table) - 1)
+    
+    def update(self, key, update_func):
+        """Update a value using a function that takes the old value and returns a new one"""
+        j = self._hash_function(key)
+        bucket = self._table[j]
+        if bucket is not None:
+            try:
+                old_value = bucket.get(key)
+                new_value = update_func(old_value)
+                bucket.put(key, new_value)
+                return True
+            except KeyError:
+                return False
+        return False
                 
     def remove(self, key):
         """Remove item associated with key"""
@@ -102,6 +137,41 @@ class ChainHashMap:
         self._n = 0
         for (k, v) in old:
             self.put(k, v)
+    
+    def __contains__(self, key):
+        """Support for 'in' operator"""
+        j = self._hash_function(key)
+        bucket = self._table[j]
+        return bucket is not None and key in bucket
+    
+    def items(self):
+        """Return a list of all (key, value) pairs in the map"""
+        result = []
+        for bucket in self._table:
+            if bucket is not None:
+                result.extend(bucket.items())
+        return result
+    
+    def keys(self):
+        """Return a list of all keys in the map"""
+        result = []
+        for bucket in self._table:
+            if bucket is not None:
+                result.extend(bucket.keys())
+        return result
+    
+    def values(self):
+        """Return a list of all values in the map"""
+        result = []
+        for bucket in self._table:
+            if bucket is not None:
+                result.extend(bucket.values())
+        return result
             
     def __len__(self):
         return self._n
+    
+    def clear(self):
+        """Remove all items from the map"""
+        self._table = len(self._table) * [None]
+        self._n = 0
