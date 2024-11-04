56508beaa9a22581777352c65f50a1454b217b72
NarainBK <bknarains@gmail.com>
2024-11-04 14:55:55 +0530

    Added time complexities to functions in hash_map.py

--- hash_map.py
+++ hash_map.py
@@ -30,6 +30,7 @@ class Entry:
     """ 
     Compares two entries based on their keys.
     This func is called when the '==' operator is encountered.
+    Time Complexity: O(1)
     """
     def __eq__(self, other):
         if isinstance(other, Entry):
@@ -38,7 +39,10 @@ class Entry:
             return self._key == other._key
         return False
     
-    # This function returns hash value of an entry.
+    """ 
+    This function returns hash value of an entry.
+    Time Complexity: O(1)
+    """
     def __hash__(self):
         return hash(self._key)
 
@@ -75,14 +79,20 @@ class UnsortedTableMap:
     def __init__(self):
         self._table = []
         
-    # This function returns value for given key   
+    """ 
+    This function returns value for given key.
+    Time Comeplexity: O(n)
+    """   
     def get(self, k):
         for item in self._table:
             if k == item._key:
                 return item._value
         raise KeyError('Key Error: ' + repr(k))
         
-    # This function inserts or updates key-value pair (k:v).
+    """ 
+    This function inserts or updates key-value pair (k:v).
+    Time Complexity: O(n)
+    """
     def put(self, k, v):
         for item in self._table:
             if k == item._key:
@@ -90,14 +100,20 @@ class UnsortedTableMap:
                 return
         self._table.append(Entry(k, v))
     
-    # This function removes an entry for a given key and returns its value
+    """ 
+    This function removes an entry for a given key and returns its value.
+    Time Complexity: O(n)
+    """
     def remove(self, k):
         for j in range(len(self._table)):
             if k == self._table[j]._key:
                 return self._table.pop(j)._value
         raise KeyError('Key Error: ' + repr(k))
     
-    #This function checks if a given key exists in hashmap
+    """
+    This function checks if a given key exists in hashmap.
+    Time Complexity: O(n)
+    """
     def __contains__(self, k):
         try:
             self.get(k)
@@ -105,19 +121,31 @@ class UnsortedTableMap:
         except KeyError:
             return False
     
-    # Function to get number of entries in map 
+    """
+    Function to get number of entries in map .
+    Time Complexity: O(1)
+    """
     def __len__(self):
         return len(self._table)
         
-    # Function to get list of all key-value pairs   
+    """ 
+    Function to get list of all key-value pairs.
+    Time Complexity: O(n)
+    """       
     def items(self):
         return [(item._key, item._value) for item in self._table]
     
-    # Function to get list of all keys
+    """
+    Function to get list of all keys.
+    Time Complexity: O(n)
+    """
     def keys(self):
         return [item._key for item in self._table]
     
-    # Function to get list of all values
+    """ 
+    Function to get list of all values.
+    Time Complexity: O(n)
+    """
     def values(self):
         return [item._value for item in self._table]
 
@@ -176,11 +204,17 @@ class ChainHashMap:
         self._shift = randrange(p)          # shift factor       
         self._load_factor_threshold = 0.5   # load factor threshold
     
-    # This function returns the hash value of a key using MAD compression method.
+    """ 
+    This function returns the hash value of a key using MAD compression method.
+    Time Complexity: O(1)
+    """
     def _hash_function(self, k):
         return (hash(k) * self._scale + self._shift) % self._prime % len(self._table)
     
-    # This function returns value for given key
+    """ 
+    This function returns value for given key.
+    Time Complexity: O(1)
+    """
     def get(self, key):
         """Retrieve value associated with key"""
         j = self._hash_function(key)
@@ -194,7 +228,8 @@ class ChainHashMap:
 
     """ This function inserts or updates key-value pair
         and increases overall map size if load factor exceeds threshold 
-        and resizes the bucket array if it exceeds capacity"""
+        and resizes the bucket array if it exceeds capacity.
+        Time Complexity: O(1)"""
     def put(self, key, value):
         """Insert or update key-value pair"""
         j = self._hash_function(key)
@@ -210,8 +245,9 @@ class ChainHashMap:
             if self._n > len(self._table) * self._load_factor_threshold:
                 self._resize(2 * len(self._table) - 1)
     
+    """Updates a value using a function that takes the old value and returns a new one.
+        Time Complexity: O(1)"""
     def update(self, key, update_func):
-        """Update a value using a function that takes the old value and returns a new one"""
         j = self._hash_function(key)
         bucket = self._table[j]
         if bucket is not None:
@@ -223,9 +259,10 @@ class ChainHashMap:
             except KeyError:
                 return False
         return False
-                
+
+    """Remove item associated with key.
+    Time Complexity: O(1)"""             
     def remove(self, key):
-        """Remove item associated with key"""
         j = self._hash_function(key)
         bucket = self._table[j]
         if bucket is None:
@@ -236,9 +273,10 @@ class ChainHashMap:
             return value
         except KeyError:
             return None
-            
+
+    """Resize bucket array to capacity c.
+    Time Complexity: O(n)"""        
     def _resize(self, c):
-        """Resize bucket array to capacity c"""
         old = []
         for bucket in self._table:
             if bucket is not None:
@@ -248,40 +286,51 @@ class ChainHashMap:
         for (k, v) in old:
             self.put(k, v)
     
+    """Support for the 'in' operator.
+    Time Complexity: O(1)"""
     def __contains__(self, key):
-        """Support for 'in' operator"""
         j = self._hash_function(key)
         bucket = self._table[j]
         return bucket is not None and key in bucket
     
+    """Return a list of all (key, value) pairs in the map.
+    Time Complexity: O(n)"""
     def items(self):
-        """Return a list of all (key, value) pairs in the map"""
         result = []
         for bucket in self._table:
             if bucket is not None:
                 result.extend(bucket.items())
         return result
     
+    """Return a list of all keys in the map.
+    Time Complexity: O(n)"""
     def keys(self):
-        """Return a list of all keys in the map"""
         result = []
         for bucket in self._table:
             if bucket is not None:
                 result.extend(bucket.keys())
         return result
     
+    """Return a list of all values in the map.
+    Time Complexity: O(n)"""
     def values(self):
-        """Return a list of all values in the map"""
         result = []
         for bucket in self._table:
             if bucket is not None:
                 result.extend(bucket.values())
         return result
-            
+
+    """ 
+    This function returns number of entries in the map.
+    Time Complexity: O(1)
+    """        
     def __len__(self):
         return self._n
     
+    """
+    This function clears the map. It resets it to initial empty state.
+    Time Complexity: O(n)
+    """
     def clear(self):
-        """This method removes all items from the map"""
         self._table = len(self._table) * [None]
         self._n = 0
